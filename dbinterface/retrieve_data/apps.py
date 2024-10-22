d9113257637e320716dc3b88dbb60bf12ded3c44
khind <kevinhind2000@yahoo.co.uk>
2024-10-23 00:42:29 +0100

    First commit of sample django project for use with EEB database.

---
+++ dbinterface/retrieve_data/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RetrieveDataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'retrieve_data'
---
+++ tests/testfiles/test_data.py
@@ -0,0 +1,5 @@
+eeb_record = [
+    ('example1', 'Notes on language', '1563'),
+    ('example2', 'More notes on language', '1565'),
+    ('example3', 'La langue francoise', '1568')
+]
